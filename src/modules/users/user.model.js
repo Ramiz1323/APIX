25b5f539ef0d89b3313622caa5ae7a602313beea
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-09-05 11:00:00 +0530

    feat(users): add bcrypt password hashing hook and matchPassword method to user model

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -35,7 +35,21 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-// Pre-save hook for password hashing will go here later
+const bcrypt = require('bcryptjs');
+
+// Hash password before saving
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) {
+        return;
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
